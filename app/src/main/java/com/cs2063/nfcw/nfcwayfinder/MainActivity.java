f76f996fbb5f7a88a0d056ce61e99f6c4b62e9a1
RBeaulieu <beaulieu_raph@outlook.com>
2016-02-05 11:13:40 -0400

    Started work on main activity using tutorial (for testing purposes)
    http://www.survivingwithandroid.com/2015/03/nfc-in-android-ndef.html

--- app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
+++ app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
@@ -1,5 +1,9 @@
 package com.cs2063.nfcw.nfcwayfinder;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent nfcIntent = new Intent(this, getClass());
+
+        nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+        IntentFilter tagIntentFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try
+        {
+            tagIntentFilter.addDataType("text/plain");
+            intentFiltersArray = new IntentFilter[]{tagIntentFilter};
+        }
+        catch (Throwable t) { t.printStackTrace(); }
     }
 }
