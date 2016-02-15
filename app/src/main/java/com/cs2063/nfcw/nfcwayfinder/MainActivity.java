1c98226bba2283c84c9abb1387d02ef10bf6d76e
Matthew Koval <d8t7n@unb.ca>
2016-02-14 20:21:06 -0400

    Pre Pull from master again, tried switching from AppCompatActivity to standard activity.

--- app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
+++ app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
@@ -1,13 +1,34 @@
 package com.cs2063.nfcw.nfcwayfinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+    private static final String TAG = "MainActivity";
+
+    private Button btnDestination;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Handle buttons and on click events.
+        btnDestination = (Button) findViewById(R.id.btnDestination);
+        if(btnDestination != null)
+        {
+            btnDestination.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(TAG, "onClick() called.");
+                }
+            });
+        }
     }
 }
