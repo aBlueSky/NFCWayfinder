5db4c8e2bba8dd3be6274626a0d7c40c9d160464
Matthew Koval <d8t7n@unb.ca>
2016-02-14 22:16:24 -0400

    Fixed example of foreground-dispatch nfc code. Updated readme with links.

--- app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
+++ app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
@@ -1,55 +1,77 @@
 package com.cs2063.nfcw.nfcwayfinder;
 
 import android.app.Activity;
-=======
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
->>>>>>> cb08f6c7f1a010f477640750fa3b78b0e68b93ee
+import android.nfc.NfcManager;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
     private static final String TAG = "MainActivity";
-
-    private Button btnDestination;
-    private Button btn2;
+    private NfcAdapter nfcAdpt;
+    private PendingIntent nfcPendingIntent;
+    private IntentFilter[]  intentFiltersArray;
+    private String[][] mTechLists;//what is mTechLists for?
+    private TextView mText;
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-        //Handle buttons and on click events.
-        btnDestination = (Button) findViewById(R.id.btnDestination);
-        if(btnDestination != null)
-        {
-            btnDestination.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.i(TAG, "onClick() called.");
-                }
-            });
-        }
-=======
-        Intent nfcIntent = new Intent(this, getClass());
+        nfcAdpt = NfcAdapter.getDefaultAdapter(this);
+        // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
+        // will fill in the intent with the details of the discovered tag before delivering to
+        // this activity.
+        nfcPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-        nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        nfcPendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+        // TODO limit the ndef MIME filter to what we need.
         IntentFilter tagIntentFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        try
-        {
+        try {
             tagIntentFilter.addDataType("text/plain");
-            intentFiltersArray = new IntentFilter[]{tagIntentFilter};
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
         }
-        catch (Throwable t) { t.printStackTrace(); }
->>>>>>> cb08f6c7f1a010f477640750fa3b78b0e68b93ee
+        intentFiltersArray = new IntentFilter[] {
+                tagIntentFilter,
+        };
+
+        // Setup a tech list for all NfcF tags
+        mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() called.");
+        super.onResume();
+        if (nfcAdpt != null) nfcAdpt.enableForegroundDispatch(this, nfcPendingIntent, intentFiltersArray,
+                mTechLists);
+    }
+
+    private void handleIntent(Intent intent)
+    {
+        Log.d(TAG, "Handling nfc intent.");
+        //handle the intent
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nfcAdpt != null) nfcAdpt.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
+        mText.setText("Discovered tag " + ++mCount + " with intent: " + intent);
     }
 }
