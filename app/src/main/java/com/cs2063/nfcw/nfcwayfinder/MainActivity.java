5a765a988738113799629442dfd925d17a6fcbb7
aBlueSky <d8t7n@unb.ca>
2016-02-21 18:00:04 -0400

    Now able to see the content of a MIME_TEXT_PLAIN ("text/plain") tag in Log.d output. Need to ensure no blocking of the UI thread and link Drew's portion (JSON parser) to the NfcReaderTask.

--- app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
+++ app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainActivity.java
@@ -5,14 +5,20 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity
 {
     private static final String TAG = "MainActivity";
+    public static final String MIME_TEXT_PLAIN = "text/plain";
     private NfcAdapter nfcAdpt;
     private PendingIntent nfcPendingIntent;
     private IntentFilter[]  intentFiltersArray;
@@ -36,7 +42,7 @@ public class MainActivity extends Activity
         // TODO limit the ndef MIME filter to what we need.
         IntentFilter tagIntentFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
         try {
-            tagIntentFilter.addDataType("text/plain");
+            tagIntentFilter.addDataType(MIME_TEXT_PLAIN);
         } catch (IntentFilter.MalformedMimeTypeException e) {
             throw new RuntimeException("fail", e);
         }
@@ -56,10 +62,53 @@ public class MainActivity extends Activity
                 mTechLists);
     }
 
+    // This method is called when an NFC tag has been registered by foreground dispatch, send to
+    // process upon successful tags.
     private void handleIntent(Intent intent)
     {
         Log.d(TAG, "Handling nfc intent.");
         //handle the intent
+        if(intent == null) return; //Not actually an intent, dismiss.
+
+        String action = intent.getAction();
+        Log.d(TAG, "Action: "+action);
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action))
+        {
+            Log.d(TAG, "Action-NDEF-Discovered.");
+            String type = intent.getType();
+            if(MIME_TEXT_PLAIN.equals(type))
+            {
+                Log.d(TAG, "MIME type: " + MIME_TEXT_PLAIN);
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                Log.d(TAG, "NDEF tag able to be executed.");
+                new NdefReaderTask().execute(tag);
+            }
+            else
+            {
+                Log.d(TAG, "Wrong MIME type.");
+            }
+        }
+        else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
+        {
+            Log.d(TAG, "Action-TECH-Discovered.");
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techListStrings = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech: techListStrings)
+            {
+                if (searchedTech.equals(tech))
+                {
+                    Log.d(TAG, "tech tag able to be executed.");
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Log.d(TAG, "Action not handled.");
+        }
     }
 
     @Override
@@ -68,11 +117,13 @@ public class MainActivity extends Activity
         if (nfcAdpt != null) nfcAdpt.disableForegroundDispatch(this);
     }
 
+    /* Where new intents are registered and able to be sent to be handled. */
     @Override
     public void onNewIntent(Intent intent)
     {
         Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
         if((mText = (TextView) findViewById(R.id.nfcTagText)) != null)
             mText.setText("Discovered tag " + ++mCount + " with intent: " + intent);
+        handleIntent(intent);
     }
 }
