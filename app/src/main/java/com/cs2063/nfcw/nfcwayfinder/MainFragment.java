2d66715b48079744c7b95225bae6410eede7580e
RBeaulieu <beaulieu_raph@outlook.com>
2016-03-13 19:49:33 -0300

    Minor tweaks

--- app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainFragment.java
+++ app/src/main/java/com/cs2063/nfcw/nfcwayfinder/MainFragment.java
@@ -2,6 +2,7 @@ package com.cs2063.nfcw.nfcwayfinder;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,12 @@ import android.view.ViewGroup;
  * Created by beaul on 2016-03-13.
  */
 public class MainFragment extends Fragment {
+
+    private static final String TAG = "MainFragment";
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        Log.d(TAG, "Entered MainFragment onCreateView");
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
